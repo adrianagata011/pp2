80b2c1e0904296a30e7bc3e19d6f6772b706da3c
adrianagata011 <adrianagata@gmail.com>
2024-05-12 13:28:45 -0300

    .

--- test/logout.php
+++ test/logout.php
@@ -18,6 +18,6 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 // Redireccionar a la página de inicio de sesión
-header("Location: login.php");
+header("Location: index.php");
 exit();
 ?>
