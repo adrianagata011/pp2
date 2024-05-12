a99c1ef62136486b7a2994edca0b21645800af17
adrianagata011 <adrianagata@gmail.com>
2024-05-12 13:14:57 -0300

    update

---
+++ test/logout.php
@@ -0,0 +1,23 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+// Destruir todas las variables de sesión
+$_SESSION = array();
+
+// Borrar la cookie de sesión
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destruir la sesión
+session_destroy();
+
+// Redireccionar a la página de inicio de sesión
+header("Location: login.php");
+exit();
+?>
