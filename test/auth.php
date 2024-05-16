98eaf425ec4bc32417f65301d2a9044d81e7b31c
adrianagata011 <adrianagata@gmail.com>
2024-05-15 23:44:50 -0300

    .

--- test/auth.php
+++ test/auth.php
@@ -22,8 +22,6 @@ $select_result = $mysqli->query("SELECT @existe AS existe");
 $row = $select_result->fetch_assoc();
 $existe = $row['existe'];
 
-// Cerrar conexión
-$mysqli->close();
 // Verificar el resultado del procedimiento almacenado
 if ($existe == 1) {
     // Iniciar sesión
@@ -31,17 +29,24 @@ if ($existe == 1) {
     $select_result_rol = $mysqli->query("SELECT rol_id FROM usuarios WHERE usuario = $usuario");
     $row = $select_result_rol->fetch_assoc();
     $rol = $row['rol_id'];
-    
+
     $_SESSION['usuario'] = $usuario;
     $_SESSION['rol_id'] = $rol
-
+    // Cerrar conexión
+    $mysqli->close();
     // Redireccionar a la pantalla de menú
     header("Location: index.php");
+
     exit();
 } else {
+    // Cerrar conexión
+    $mysqli->close();
     // Mostrar mensaje de error y redireccionar de nuevo a la página de inicio de sesión
     echo "Usuario o contraseña incorrectos. Inténtelo nuevamente.";
     header("refresh:3; url=login.html");
+
+
+
     exit();
 }
 ?>
