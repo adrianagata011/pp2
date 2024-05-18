14987ddf5a2057fe22662fc1e99798b8e319fc71
adrianagata011 <adrianagata@gmail.com>
2024-05-18 16:38:55 -0300

    commit

---
+++ test/Database/Dtabase.sql
@@ -0,0 +1,46 @@
+-- mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'testing01';
+-- mysql> ALTER USER 'pp2'@'%' IDENTIFIED BY 'Testing_2024';
+
+use PP2;
+
+DROP TABLE usuarios;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    rol INT NOT NULL
+);
+
+INSERT INTO usuarios (usuario,contrasena,rol) values ('paciente','password01',1);
+INSERT INTO usuarios (usuario,contrasena,rol) values ('administrador','password01',2);
+
+drop procedure if exists VerificarUsuario;
+
+delimiter //
+CREATE PROCEDURE VerificarUsuario(
+    IN t_usuario varchar(50),
+    IN t_contrasena varchar(255),
+    OUT existe boolean,
+    OUT rol_id int
+)
+begin
+    declare t_count INT;
+    declare t_rol INT;
+    select count(*) into t_count from usuarios where usuario = t_usuario and contrasena = t_contrasena;
+    if t_count > 0 then
+        set existe = TRUE;
+        select CONVERT(rol, SIGNED INTEGER) into t_rol from usuarios where usuario = t_usuario;
+        set rol_id = t_rol;
+	else
+        set existe = FALSE;
+        set rol_id = NULL;
+    end if;
+end
+//
+
+-- Para probar una salida verdadera y una falsa
+-- CALL VerificarUsuario('pirulo', 'gomezd01', @existe, @rol);
+-- SELECT @existe,@rol;
+-- CALL VerificarUsuario('paciente', 'password01', @existe, @rol);
+-- SELECT @existe, @rol;
