b904a559b8aad5e6ed65a8aa3ac38b2907988079
AleLoredo <34008987+AleLoredo@users.noreply.github.com>
2024-05-26 03:13:08 -0300

    Add files via upload
    Completa la funcionalidad de listar turnos del paciente.

--- test/cancelar_turno_admin.php
+++ test/cancelar_turno_admin.php
@@ -1,27 +1,15 @@
-<?php
-// Iniciar la sesión
-session_start();
-
-// Verificar si la sesión está establecida y el usuario está logueado
-if (!isset($_SESSION['usuario']) || $_SESSION['rol_id'] != 2 ) {
-    // Si no está logueado, redirigir al usuario a la página de login
-    header("Location: login.html");
-    exit;
-}
-?>
-
 <!DOCTYPE html>
-<html lang='es'>
+<html lang="es">
 
 <head>
 
-    <meta charset="iso-8859-1">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Sistema Clínica - Cancelación de Turnos</title>
+    <title>Cancelar turno</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -44,67 +32,96 @@ if (!isset($_SESSION['usuario']) || $_SESSION['rol_id'] != 2 ) {
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                        <!--<div class="row"> -->
-                        <!--<div class="col-lg-6"> -->
+                        <!-- //<div class="row">
+                            //<div class="col-lg-6"> -->
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Cancelación de Turnos</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Cancelar turnos</h1>
                                     </div>
                                     <hr>      
                                     <div class="text-center">
-                                        <form action="cancelar_turno_admin1.php" method="GET">
-                                            <div class="text-center">
+                                    <form action="cancelar_turno.php" method="GET">
+                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                    <th>Seleccionar</th>
+                                                    <th>Fecha</th>
+                                                    <th>Servicio</th>
+                                                    <th>Profesional</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
 <?php
-// Me conecto a la base
-$mysqli = new mysqli('sql10.freemysqlhosting.net', 'sql10707793', 'Rre1s76tSV', 'sql10707793');
-// Verificar conexión
-if ($mysqli->connect_error) {
-    //die("Error en la conexión: " . $mysqli->connect_error);
-}
-    
-$query = "SELECT idUsuario, nombre, apellido, dni FROM pacientes ORDER BY apellido;";
-$result = $mysqli->query($query);
-if ($result->num_rows > 0) {
-    echo "<tr>";
-//    echo "<label for='pacientes'>Selecciones un Paciente:</label>";
-    echo "<select name='usuario' id=usuario'>";
-    // Output de cada fila
-    while($row = $result->fetch_assoc()) {
-        $idUsuario = $row['idUsuario'];
-        $nombre = $row['nombre'];
-        $apellido = $row['apellido'];
-        $dni = $row['dni'];
-        echo "<option value='".$idUsuario."'>".$apellido.", ".$nombre." (DNI ". $dni . ")</option>";
+
+use LDAP\Result;
+
+if (isset($_GET['dni'])) {
+    $dni = $_GET['dni'];
+
+    // Como sabemos que el usuario existe en la base de datos porque ya fue chequeado en userdni.php entonces
+    // 1- Me conecto a la base
+    $conn = new mysqli('sql10.freemysqlhosting.net', 'sql10707793', 'Rre1s76tSV', 'sql10707793');
+    // 2- Verifico la conexión
+    if ($conn->connect_error) {
+        die("Error en la conexión: " . $conn->connect_error);
     }
-    echo "</select><br>";
-} else {
-    echo "No se encontró ningún paciente<br>";
-}
-$mysqli->close();
+    // 3- Como la conexion fue exitosa seteo el charset a utf8
+    $conn->set_charset("utf8");
+    
+    $query2 = "SELECT t.fechaHora as fecha, s.nombre as servicio, p.nombre as nombre, p.apellido as apellido 
+        FROM turnos t 
+        INNER JOIN profesionales p ON t.idProfesional = p.idProfesional 
+        INNER JOIN servicios s ON t.idServicio = s.idServicio 
+        INNER JOIN pacientes pa ON t.idPaciente = pa.idPaciente 
+        INNER JOIN usuarios u ON pa.idUsuario = u.idUsuario 
+        WHERE pa.dni = $dni 
+        ORDER BY t.fechaHora ASC;";
+    
+        $result = mysqli_query($conn, $query2);
+
+            if ($result && mysqli_num_rows($result) > 0) {
+                // Output de cada fila
+                while($row = mysqli_fetch_assoc($result)) {
+                    $fecha = $row['fecha'];
+                    $servicio = $row['servicio'];
+                    $profesional = $row['nombre'] . " " . $row['apellido'];
+                    echo "<tr><td><input type='radio' name='turno' value= '" . $fecha . "'></td><td>" . $fecha . "</td><td>" . $servicio . "</td><td>" . $profesional . "</td></tr>";
+                }           
+            } 
+            else {
+                echo "<tr><td>No se encontraron resultados</td><td></td><td></td></tr>";
+            }
+        }
+        else {
+            echo "<tr><td>No se pudo identificar el ID de Usuario del paciente</td><td></td><td></td></tr>";
+        }
 ?>
-                                        </div>
-                                        <div>
-                                            <br>
-                                            <button type="submit" class="btn btn-primary btn-user btn-block">
-                                                seleccionar paciente
-                                            </a>
-                                        </div>
+
+                                            </tbody>
+                                        </table>
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                             Cancelar turno
+                                        </button>
+                                    <hr>
                                     </form>
+                                    </div>
                                     <div>
-                                        <hr>
-                                        <a class="btn btn-primary btn-user btn-block" href="index_administrativo.php">
-                                            volver
+
+                                    <hr>
+
+                                        <a href="index_paciente.php" class="btn btn-primary btn-user btn-block" href="#" data-toggle="modal" data-target="#logoutModal">
+                                            Salir
                                         </a>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            <!-- //</div>
+                        //</div> -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 
     <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
@@ -117,10 +134,10 @@ $mysqli->close();
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">¿Está seguro que desea cancelar el turno?</div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">volver</button>
-                    <a class="btn btn-primary" href="cancelar_turno_t.php">Cancelar Turno</a>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
                 </div>
             </div>
         </div>
@@ -139,3 +156,5 @@ $mysqli->close();
 </body>
 
 </html>
+
+
