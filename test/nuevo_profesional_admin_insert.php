0b075cd54ab1511a402f8b2a0f346f7d01fdd7b5
adrianagata011 <adrianagata@gmail.com>
2024-05-29 23:57:17 -0300

    update

--- test/nuevo_profesional_admin_insert.php
+++ test/nuevo_profesional_admin_insert.php
@@ -1,59 +1,57 @@
 <?php
-session_start();
+require_once('verificar_sesion_admin.php');
+
+if (!isset($_POST['nombre']) || empty($_POST['nombre']) || !isset($_POST['apellido']) || empty($_POST['apellido']) || !isset($_POST['dni']) || empty($_POST['dni']) || !isset($_POST['numeroMatricula']) || empty($_POST['numeroMatricula']) || !isset($_POST['horarioIngreso']) || empty($_POST['horarioIngreso']) || !isset($_POST['horarioEgreso']) || empty($_POST['horarioEgreso']) ||!isset($_POST['inicioActividad']) || empty($_POST['inicioActividad'])) {
+    echo "Uno de los campos obligatorios está vacío:<br>";
+    echo "- nombre<br>";
+    echo "- apellido<br>";
+    echo "- dni<br>";
+    echo "- Servicio<br>";
+    echo "- numero de Matricula<br>";
+    echo "- horario de Ingreso<br>";
+    echo "- horario de Engreso<br>";
+    echo "- inicio de Actividad<br>";
+    header("refresh:3; url=nuevo_profesional_admin.php");
+    exit();
+}
 
 // Obtener los datos del formulario
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $dni = $_POST['dni'];
-$telefono = $_POST['telefono'];
-$domicilio = $_POST['domicilio'];
-$email = $_POST['email'];
+if (!isset($_POST['telefono']) || empty($_POST['telefono'])) {
+    $telefono = $_POST['telefono'];
+} else {
+    $telefono = '';
+}
+if (!isset($_POST['domicilio']) || empty($_POST['domicilio'])) {
+    $domicilio = $_POST['domicilio'];
+} else {
+    $domicilio = '';
+}
+if (!isset($_POST['email']) || empty($_POST['email'])) {
+    $email = $_POST['email'];
+} else {
+    $email = '';
+}
+$idServicio = $_POST['idServicio'];
 $numeroMatricula = $_POST['numeroMatricula'];
 $horarioIngreso = $_POST['horarioIngreso'];
 $horarioEgreso = $_POST['horarioEgreso'];
 $inicioActividad = $_POST['inicioActividad'];
 
-// Conectar a la base de datos
-// $mysqli = new mysqli('localhost', 'pp2', 'Testing_2024', 'pp2');
 $mysqli = new mysqli('sql10.freemysqlhosting.net', 'sql10707793', 'Rre1s76tSV', 'sql10707793');
 
-// Verificar conexión
 if ($mysqli->connect_error) {
     die("Error en la conexión: " . $mysqli->connect_error);
 }
-// comentario
-// Llamar al procedimiento almacenado VerificarUsuario
-$query = "CALL VerificarUsuario('$usuario', '$contrasena', @existe, @rol)";
-$result = $mysqli->query($query);
 
+$query = "INSERT INTO profesionales (idServicio, nombre, apellido, dni, telefono, domicilio, email, numeroMatricula, horarioIngreso, horarioEgreso, inicioActividad) VALUES ($idServicio, '$nombre', '$apellido', '$dni', '$telefono', '$direccion', '$email', '$numeroMatricula', 'horarioIngreso', 'horarioEgreso', '$inicioActividad');";
+$result = $mysqli->query($query);
 // Obtener el resultado del procedimiento almacenado
-$select_result = $mysqli->query("SELECT @existe AS existe, @rol AS rol");
-$row = $select_result->fetch_assoc();
-$existe = $row['existe'];
-$rol = $row['rol'];
 $mysqli->close();
 
-// Verificar el resultado del procedimiento almacenado
-if ($existe == 1) {
-    // Iniciar sesión
-    $_SESSION['usuario'] = $usuario;
-    $_SESSION['rol_id'] = $rol;
-    // Redireccionar a la pantalla de menú
-    if ($rol == 1) {
-        header("Location: index_paciente.php");
-        exit();
-    }
-    else 
-    {
-        header("Location: index_administrativo.php");
-        exit();        
-    }
-    
-
-} else {
-    // Mostrar mensaje de error y redireccionar de nuevo a la página de inicio de sesión
-    echo "Usuario o contraseña incorrectos. Inténtelo nuevamente.";
-    header("refresh:3; url=login.html");
-    exit();
-}
+echo "Se ingresó el Profesional Nuevo";
+header("refresh:3; url=index_administrativo.php");
+exit();
 ?>
