86fff5d131588ff9ee8a87dd5736d23b0d7ec924
adrianagata011 <adrianagata@gmail.com>
2024-06-02 23:27:39 -0300

    udpate

--- test/admin_modificar_paciente.php
+++ test/admin_modificar_paciente.php
@@ -1,10 +1,6 @@
 <?php
 // usar si es una pagina para el admin
 require_once('verificar_sesion_admin.php');
-
-// y usar este si es una pagina para el paciente
-// require_once('verificar_sesion_paciente.php');
-
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +14,7 @@ require_once('verificar_sesion_admin.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Sistema Clínica - Modificación Profesional</title>
+    <title>Sistema Clínica - Modificar Paciente</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -44,47 +40,152 @@ require_once('verificar_sesion_admin.php');
 
                             <!-- Columna simple centrada con Card Body -->
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Modificación Profesional</h1>
+                                <h1 class="h4 text-gray-900 mb-4">Modificar Paciente</h1>
                             </div>
                             <hr>
-                            <div>                            
+
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">DNI <?php echo $_GET['dni']; ?></h1>
+                            </div>
+
+                            <?php
+if (isset($_GET['dni'])) {
+    $dni = $_GET['dni'];
+    // Me conecto a la base
+    require_once('conexion_db.php');
+    $query = "SELECT idFichaMedica,idPaciente,idUsuario,nombre,apellido,telefono,domicilio,email,obraSocial from pacientes where dni = '$dni';";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $idFichaMedica = $row['idFichaMedica'];
+        $idPaciente = $row['idPaciente'];
+        $idUsuario = $row['idUsuario'];
+        $nombre = $row['nombre'];
+        $apellido = $row['apellido'];
+        $telefono = $row['telefono'];
+        $domicilio = $row['domicilio'];
+        $email = $row['email'];
+        $obraSocial = $row['obraSocial'];
+        $query1 = "SELECT grupoSanguineo,observaciones from fichas_medicas where idFichaMedica = '$idFichaMedica';";
+        $result = $conn->query($query1);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $grupoSanguineo = $row['grupoSanguineo'];
+            $observaciones = $row['observaciones'];
+        }
+        $query2 = "SELECT usuario,contrasena from usuarios where idUsuario = '$idUsuario';";
+        $result = $conn->query($query2);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $usuario = $row['usuario'];
+            $contrasena = $row['contrasena'];
+        }
+    }
+    else 
+    {
+        echo "No se seleccionó el profesional<br>";
+    }
+    $conn->close();
+}
+?>
+
+                            <form class="user" method="post" action="admin_modificar_paciente_update.php">
+                                <div class="form-group">
+                                    <input type="nombre" class="form-control form-control-user"
+                                        id="nombre" name="nombre" aria-describedby="emailHelp"
+                                        value="<?php echo $nombre; ?>"
+                                        placeholder="Ingrese el nombre">
+                                </div>
+                                <div class="form-group">
+                                    <input type="apellido" class="form-control form-control-user"
+                                        id="apellido" name="apellido" aria-describedby="emailHelp"
+                                        value="<?php echo $apellido; ?>"
+                                        placeholder="Ingrese el apellido">
+                                </div>
+                                <div class="form-group">
+                                    <input type="telefono" class="form-control form-control-user"
+                                        id="telefono" name="telefono" aria-describedby="emailHelp"
+                                        value="<?php echo $telefono; ?>"
+                                        placeholder="Ingrese el teléfono">
+                                </div>
+                                <div class="form-group">
+                                    <input type="domicilio" class="form-control form-control-user"
+                                        id="domicilio" name="domicilio" aria-describedby="emailHelp"
+                                        value="<?php echo $domicilio; ?>"
+                                        placeholder="Ingrese el domicilio">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user"
+                                        id="email" name="email" aria-describedby="emailHelp"
+                                        value="<?php echo $email; ?>"
+                                        placeholder="Ingrese e-mail">
+                                </div>
+                                <div class="form-group">
+                                    <input type="obraSocial" class="form-control form-control-user"
+                                        id="obraSocial" name="obraSocial" aria-describedby="emailHelp"
+                                        value="<?php echo $obraSocial; ?>"
+                                        placeholder="Ingrese la Obra Social">
+                                </div>
+                                <div class="form-group">
+                                    <label for="grupoSanguineo"> Seleccione el Grupo Sanguineo: </label>
+                                    <select name="grupoSanguineo" id="grupoSanguineo">
+<?php
+if ($grupoSanguineo == 'A+') {echo "<option value='A+' selected>A+</option>";} else {echo "<option value='A+'>A+</option>";}
+if ($grupoSanguineo == 'A-') {echo "<option value='A-' selected>A-</option>";} else {echo "<option value='A-'>A-</option>";}
+if ($grupoSanguineo == 'B+') {echo "<option value='B+' selected>B+</option>";} else {echo "<option value='B+'>B+</option>";}
+if ($grupoSanguineo == 'B-') {echo "<option value='B-' selected>B-</option>";} else {echo "<option value='B-'>B-</option>";}
+if ($grupoSanguineo == 'AB+') {echo "<option value='AB+' selected>AB+</option>";} else {echo "<option value='AB+'>AB+</option>";}
+if ($grupoSanguineo == 'AB-') {echo "<option value='AB-' selected>AB-</option>";} else {echo "<option value='AB-'>AB-</option>";}
+if ($grupoSanguineo == 'O+') {echo "<option value='O+' selected>O+</option>";} else {echo "<option value='O+'>O+</option>";}
+if ($grupoSanguineo == 'O-') {echo "<option value='O-' selected>O-</option>";} else {echo "<option value='O-'>O-</option>";}
+?>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="observaciones" class="form-control form-control-user"
+                                        id="observaciones" name="observaciones" aria-describedby="emailHelp"
+                                        value="<?php echo $observaciones; ?>"
+                                        placeholder="Observaciones">
+                                </div>
+                                <hr>
+                                <div class="text-center">
+                                    <h6 class="h6 text-gray-900 mb-6">Datos del Sistema</h6>
+                                </div>
+                                <div class="text-center">
+                                    <table width="100%">
+                                        <tr>
+                                            <td>
+                                                <input type="usuario" class="form-control form-control-user"
+                                                id="usuario" name="usuario" aria-describedby="emailHelp"
+                                                value="<?php echo $usuario; ?>"
+                                                placeholder="Usuario" readonly>
+                                            </td>
+                                            <td>
+                                                <input type="contrasena" class="form-control form-control-user"
+                                                id="contrasena" name="contrasena" aria-describedby="emailHelp"
+                                                value="<?php echo $contrasena; ?>"
+                                                placeholder="Contraseña">
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <hr>
+                                <input type="hidden" id="dni" name="dni" value="<?php echo $_GET['dni']; ?>">
+                                <button type="submit" value="Iniciar Sesion" class="btn btn-primary btn-user btn-block"> Grabar Modificaciones </button>
+                            </form>
+                            <hr>
+                            <div class="form-group">                            
                                 <a href="index_administrativo.php" class="btn btn-primary btn-user btn-block">
-                                    Home
-                                </a>
-                                <a class="btn btn-primary btn-user btn-block" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    Salir
+                                    Volver
                                 </a>
                             </div>
                         </div>
-
-
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -96,7 +197,27 @@ require_once('verificar_sesion_admin.php');
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
 
+    <script>
+        document.getElementById('horarioIngreso').selectedIndex = 0;
+        document.getElementById('horarioEgreso').selectedIndex = 23;
+        document.getElementById('horarioIngreso').addEventListener('change', validateSelects);
+        document.getElementById('horarioEgreso').addEventListener('change', validateSelects);
+
+        function validateSelects() {
+            var horarioIngreso = document.getElementById('horarioIngreso');
+            var horarioEgreso = document.getElementById('horarioEgreso');
+            var value1 = parseInt(horarioIngreso.value);
+            var value2 = parseInt(horarioEgreso.value);
+
+            if (value1 > value2) {
+                alert('Error: El Horario de Ingreso no puede ser mayor al Horario de Egreso');
+                // Reset both selects
+                horarioIngreso.selectedIndex = 0;
+                horarioEgreso.selectedIndex = 23;
+            }
+        }
+    </script>
+
 </body>
 
 </html>
-s
---
+++ test/admin_modificar_paciente_update.php
@@ -0,0 +1,58 @@
+<?php
+require_once('verificar_sesion_admin.php');
+
+if ( !isset($_POST['nombre']) || empty($_POST['nombre']) || !isset($_POST['apellido']) || empty($_POST['apellido']) || !isset($_POST['obraSocial']) || empty($_POST['obraSocial']) || !isset($_POST['grupoSanguineo']) || empty($_POST['grupoSanguineo']) || !isset($_POST['contrasena']) || empty($_POST['contrasena']) ) {
+    echo "Uno de los campos obligatorios está vacío:<br>";
+    echo "- nombre<br>";
+    echo "- apellido<br>";
+    echo "- obra social<br>";
+    echo "- grupo sanguineo<br>";
+    echo "- contraseña<br>";
+    $dni = $_POST['dni'];
+    header("refresh:3; url=admin_modificar_paciente.php?dni=$dni");
+    exit();
+}
+
+// Obtener los datos del formulario
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$telefono = $_POST['telefono'];
+$domicilio = $_POST['domicilio'];
+$email = $_POST['email'];
+$obraSocial = $_POST['obraSocial'];
+$grupoSanguineo = $_POST['grupoSanguineo'];
+$observaciones = $_POST['observaciones'];
+$dni = $_POST['dni'];
+$contrasena = $_POST['contrasena'];
+
+require_once('conexion_db.php');
+
+/*
+1) traigo idPaciente, idFichaMedica, idUsuario de pacientes
+2) update pacientes
+3) update fichas_medicas
+4) update usuarios
+*/
+
+$query = "SELECT idPaciente,idFichaMedica,idUsuario FROM pacientes WHERE dni = '$dni'";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$idPaciente = $row['idPaciente'];
+$idFichaMedica = $row['idFichaMedica'];
+$idUsuario = $row['idUsuario'];
+
+$query1 = "UPDATE pacientes SET nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', domicilio = '$domicilio', email = '$email', obraSocial = '$obraSocial' WHERE idPaciente = $idPaciente;";
+$result = $conn->query($query1);
+
+$query2 = "UPDATE fichas_medicas SET grupoSanguineo = '$grupoSanguineo', observaciones = '$observaciones' WHERE idFichaMedica = $idFichaMedica;";
+$result = $conn->query($query2);
+
+$query3 = "UPDATE usuarios SET contrasena = '$contrasena' WHERE idUsuario = $idUsuario;";
+$result = $conn->query($query3);
+
+$conn->close();
+
+echo "Se modificaron los datos del Paciente";
+header("refresh:3; url=index_administrativo.php");
+exit();
+?>
