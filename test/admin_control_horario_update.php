46fb6c8c3007a458c6a9ec277ea3f3b5b7769112
adrianagata011 <adrianagata@gmail.com>
2024-06-02 21:21:11 -0300

    update

--- test/admin_control_horario_update.php
+++ test/admin_control_horario_update.php
@@ -18,9 +18,49 @@ if (!isset($_POST['tipoFichada']) || empty($_POST['tipoFichada'])) {
 // Obtener los datos del formulario
 
 $idProfesional = $_POST['idProfesional'];
-$tipoFichada= $_POST['tipoFichada'];
+$tipoFichada = $_POST['tipoFichada'];
 
-if ($tipo)
+require_once('conexion_db.php');
+
+if ($tipoFichada == "ingreso"){
+    $query = "SELECT fechaHoraIngreso FROM control_horario WHERE idProfesional = $idProfesional AND DATE(fechaHoraIngreso) = CURDATE();";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        echo "Ya hay un ingreso para el día de hoy";
+        header("refresh:3; url=admin_control_horario.php?idProfesional=$idProfesional");
+        exit();
+    } else {
+        $query1 = "INSERT INTO control_horario (idProfesional, fechaHoraIngreso) VALUES ($idProfesional, NOW());";
+        $result = $conn->query($query1);
+        echo "Se fichó el ingreso";
+        header("refresh:3; url=admin_control_horario.php?idProfesional=$idProfesional");
+        exit();
+    }
+} else {
+    $query = "SELECT fechaHoraIngreso FROM control_horario WHERE idProfesional = $idProfesional AND DATE(fechaHoraIngreso) = CURDATE();";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        $query1 = "SELECT fechaHoraEgreso FROM control_horario WHERE idProfesional = $idProfesional AND DATE(fechaHoraEgreso) = CURDATE();";
+        $result = $conn->query($query1);
+        if ($result->num_rows > 0) {
+            echo "Ya hay un egreso para el día de hoy";
+            header("refresh:3; url=admin_control_horario.php?idProfesional=$idProfesional");
+            exit();
+        } else {
+            $query2 = "UPDATE control_horario SET fechaHoraEgreso = NOW() WHERE idProfesional = $idProfesional AND DATE(fechaHoraIngreso) = CURDATE();";
+            $result = $conn->query($query2);
+            echo "Se fichó el egreso";
+            header("refresh:3; url=admin_control_horario.php?idProfesional=$idProfesional");
+            exit();
+        }
+    } else {
+        echo "No hay ingreso para el día de hoy";
+        header("refresh:3; url=admin_control_horario.php?idProfesional=$idProfesional");
+        exit();
+    }    
+}
+$conn->close();
+
 // si tipoFichada = ingreso
 // reviso si ya no hay uno para el día de hoy
 // si hay rechazo, si no hay insert
@@ -31,29 +71,4 @@ if ($tipo)
 //           si hay rechazo, si no hay insert
 //      si no hay rechazo
 
-
-
-
-if ($cantidad > $cantidadExistente) {
-    echo "No se puede egresar una cantidad mayor a la Existente ($cantidadExistente)<br>";
-    $idInsumo = $_POST['idInsumo'];
-    header("refresh:3; url=admin_egresar_insumo.php?idInsumo=$idInsumo");
-    exit();
-}
-else
-{
-    $restaCantidades = $cantidadExistente - $cantidad;
-    require_once('conexion_db.php');
-    $query = "UPDATE insumos SET cantidadExistente = $restaCantidades WHERE idInsumo = $idInsumo;";
-    if ($conn->query($query) === TRUE) {
-        echo "Se actualizó la inforamción del Insumo";
-        header("refresh:3; url=index_administrativo.php");
-        exit();
-    } else {
-        echo "Error al actualizar la información del Insumo";
-        header("refresh:3; url=index_administrativo.php");
-        exit();
-    }
-    $conn->close();
-}
 ?>
